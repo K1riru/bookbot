5dd3056db1b1328660c9f4d12d893abe7ead4b3e
K1riru <kiril.boshikyov@gmail.com>
2025-08-25 17:46:39 +0000

    catch up

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def get_book_text(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-    return file_contents
+from stats import get_num_words, get_chars_dict
+
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
-    print(text)
-main()
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(f"{num_words} words found in the document")
+    print(chars_dict)
 
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
